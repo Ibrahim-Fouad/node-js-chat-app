8a4a8827828aa858739b3351283f7197438fa814
Ibrahim Fouad <iamdr.max@gmail.com>
2018-05-23 09:05:27 +0200

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,20 +3,32 @@ const http = require("http");
 const config = require('../config/config');
 const socketIO = require('socket.io');
 const app = express();
+const { generateMessage } = require('./utils/message');
+
 
 let server = http.createServer(app);
 let io = socketIO(server);
 
+let username = '';
+
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'new user joined'));
+
     socket.on('createMessage', (message) => {
         console.log('Create message event emited', message);
-        io.emit('newMessage', {message: 'Hello 2'});
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
 
     socket.on('disconnect', () => {
-        console.log('Disconnected');
+        /* socket.broadcast.emit('newMessage', {
+             from: 'Username',
+             text: 'Username has left',
+             createdAt: new Date().getTime()
+         });*/
     });
 });
 
@@ -27,7 +39,7 @@ app.use(express.static(config.PublicPath));
 
 
 server.listen(config.Port, (err) => {
-    if(err) return console.log(err);
+    if (err) return console.log(err);
 
     console.log(`Listening on port ${config.Port}`);
 });
